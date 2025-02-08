a23d2db3c1839d99a8e2aee9bd803c59f4514276
aracope <156398622+aracope@users.noreply.github.com>
2025-02-08 14:51:11 -0800

    Add event listener for keystrokes. Write search function.

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+input.addEventListener('keyup', searchHandler); // creates the event listener that triggers when user types
+
 function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+  let results = []; // Creates empty string for results
+  const lowercaseInput = str.toLowerCase(); // changes the input to all lowercase
+  const fruitResults = fruit.filter(function(val){
+	return val.toLowerCase().includes(lowercaseInput);
+  });
+  results = fruitResults;
+  return results;
 }
 
 function searchHandler(e) {
